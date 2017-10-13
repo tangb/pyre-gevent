9e5794838d0fa3ada421a1726a42027dcae98a62
tangb <2676511+tangb@users.noreply.github.com>
2017-10-13 23:20:19 +0200

    rename from pyre to pyre-gevent

--- setup.py
+++ setup.py
@@ -27,13 +27,13 @@ def find_version(*file_paths):
 
 
 setup(
-        name='pyre',
+        name='pyre-gevent',
         version=find_version('pyre', '__init__.py'),
         description='Python ZRE implementation',
         author='Arnaud Loonstra',
         author_email='arnaud@sphaero.org',
-        url='http://www.github.com/zeromq/pyre/',
-        packages=['pyre'],
+        url='http://www.github.com/tangb/pyre-gevent/',
+        packages=['pyre-gevent'],
         include_package_data=True,
         requires=['pyzmq', 'ipaddress'],
         install_requires=['pyzmq', 'ipaddress'],
