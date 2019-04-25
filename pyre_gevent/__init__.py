978d31f01c4797a8e602ec98607796b2f7a1c906
Tang <tanguy.bonneau+gitkraken@gmail.com>
2019-04-25 23:51:14 +0200

    Fix non digit in version

--- pyre_gevent/__init__.py
+++ pyre_gevent/__init__.py
@@ -1,6 +1,8 @@
+import re
+
 __all__ = ['pyre', 'zbeacon', 'zhelper']
-__version__ = '0.2.3.dev0'
-__version_info__ = tuple(int(v) for v in __version__.split('.'))
+__version__ = '0.2.3.dev1'
+__version_info__ = tuple(int(v) for v in re.sub('[a-zA-Z]', '', __version__).split('.'))
 
 from .pyre import Pyre
 from .pyre_event import PyreEvent
