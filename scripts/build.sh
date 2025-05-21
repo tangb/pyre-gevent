d980c8f8cebc851931f06944b3e926a0943093f2
Tangb <tanguy.bonneau@gmail.com>
2025-05-21 14:56:33 +0200

    Modernize build+publish process

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,7 @@
 
 cd ..
 rm -rf dist/
-python3 setup.py clean
-python3 setup.py sdist
-python3 setup.py bdist_wheel --universal
+python -m build --outdir dist/ --wheel --sdist 
 rm -rf pyre_gevent.egg-info
 rm -rf build
 cd -
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+if ! test -f ~/.pypirc; then
+  echo "~/.pypirc config file does not exist. Please add it with pyre-gevent repository entry"
+fi
+
 ./build.sh
 cd ..
-python -m twine && python -m twine upload dist/* || twine upload dist/*
+python -m twine --version && python -m twine upload --repository pyre-gevent dist/*
 cd -
 
